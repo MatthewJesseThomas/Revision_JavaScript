ead581b7485fe2e8bd9eeb6efb24cfbc81569850
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-29 15:01:19 +0200

    'main'

--- js/code.js
+++ js/code.js
@@ -1,13 +1,13 @@
 // ======Math Methods====== //
 // console.log("sqrt():",Math.sqrt(36));
 // =====Return a middle element(s)======= //
-let data = [9,3,1,"Peter",5,10];
+// let data = [9,3,1,"Peter",5,10];
 // data.splice(3,1);
-let onlyNumbers = data.filter( item => {
-    if( typeof item != 'string')
-        return item;
-});
-console.log("Max:",Math.max(...onlyNumbers));
+// let onlyNumbers = data.filter( item => {
+//     if( typeof item != 'string')
+//         return item;
+// });
+// console.log("Max:",Math.max(...onlyNumbers));
 // let size =data.length-1;
 // console.log("Array size:",size);
 // console.log("Element:",data[size]);
@@ -22,4 +22,92 @@ console.log("Max:",Math.max(...onlyNumbers));
 //     console.log(data.middleIndex);
 // }
 // ===========================================================================
+// Date //
+// let myDate = new Date();
+// console.log("date: ",myDate);
+// console.log("My date: ",myDate.toLocaleDateString());
+// console.log("My time: ",myDate.toLocaleTimeString());
+// console.log("The Month: ",myDate.getMonth());
+// console.log("The Date: ",myDate.getDate());
+// console.log("Hours: ",myDate.getHours());
+// console.log("Minutes: ",myDate.getMinutes());
+// console.log("Seconds: ",myDate.getSeconds());
+// ===========================================================================
+// Add 5Days to Date and  //
+// ===========================================================================
+// If-Statements //
+// let magic = "Tier6";
+// if( magic > "Tier6") {
+//     console.log("You are Mage");
+// }else {
+//     console.log("You are not Mage");
+// }
+// ===========================================================================
+// Nested If-Statements //
+// let age = 22;
+// let salary = 4990;
+// if(age > 21) {
+//     if(salary >= 5000) {
+//         console.log("Well Done Amigo");
+//     }else{
+//         console.log("Present The Second Payslip");
+//     }
+// }else {
+//     console.log("You are Not Qualified");
+// }
+// ===========================================================================
+// Correct way Of writing an If-Statement //
+// let age = 22;
+// let salary = 50000;
+// if((age >17) && (salary >=5000)) {
+//     console.log("Well Done");
+// }else {
+//     console.log("Thank you, for coming.");
+// }
+// ===========================================================================
+// Switch-Case Statements //
+// let monthInt = 1;
+// switch(monthInt) {
+//     case 1:
+//         console.log("January");
+//     break;
+//     case 2:
+//         console.log("Febuary");
+//     break;
+// }
+let grade = 75;
+switch(true) {
+    case grade == 100:
+        console.log("You're awesome");
+    break;
+    case (grade >= 90) && (grade <= 99):
+        console.log("Well done");
+    break;
+    case (grade >= 75) && (grade <= 89):
+        console.log("Distinction");
+    break;
+    case (grade >= 50) && (grade <= 74):
+        console.log("Pass");
+    break;
+    case (grade <=49) && (Math.sign(grade) !=-1 ):
+        console.log("Fail");
+    break;
+    default:
+        console.log("Out of range");
+}// ===========================================================================
+// AddEventListeners //
+// let grandParent = document.querySelector(".grandparent") 
+// let parent = document.querySelector(".parent") 
+// let child = document.querySelector(".child") 
+
+// grandParent.addEventListener('click', e => {
+//     console.table(e)
+// })
+// ===========================================================================
 // ======String Methods====== //
+// let sentence = "I Love Programming, I Love Cooking, I Love Swimming";
+// console.log(sentence);
+// console.log(sentence[0]);
+// console.log(sentence.toLowerCase());
+// console.log(sentence.split(', '));
+// console.log(sentence.concat("and Malware Coding"));
